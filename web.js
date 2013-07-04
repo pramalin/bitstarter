b347e7e0df3dcade05226873945dd4125f69455a
Ubuntu <ubuntu@ip-172-31-43-118.us-west-2.compute.internal>
2013-07-04 21:39:27 +0000

    Returning content from file.

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
+var Buffer = require('buffer').Buffer;
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var html = fs.readFileSync('./index.html', 'utf-8');
+  response.send(html);
 });
 
 var port = process.env.PORT || 5000;
